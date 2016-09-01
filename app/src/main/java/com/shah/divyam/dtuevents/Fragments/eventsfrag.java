fe324b4432ff03e5e1ffa27d7ed2e5be803e16d9
furyjack <lakshaytaneja26@gmail.com>
2016-09-02 04:40:51 +0530

    recyclerview of event cards added
    dummy data

--- app/src/main/java/com/shah/divyam/dtuevents/Fragments/eventsfrag.java
+++ app/src/main/java/com/shah/divyam/dtuevents/Fragments/eventsfrag.java
@@ -3,16 +3,22 @@ package com.shah.divyam.dtuevents.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.shah.divyam.dtuevents.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import java.util.ArrayList;
+
 public class eventsfrag extends Fragment {
+
+
+    ArrayList<String>evt_names;
+    RecyclerView event_list;
 
 
     public eventsfrag() {
@@ -24,7 +30,71 @@ public class eventsfrag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_eventsfrag, container, false);
+        evt_names=new ArrayList<>();
+        evt_names.add("event1");
+        evt_names.add("event2");
+        evt_names.add("event3");
+        evt_names.add("event4");
+        evt_names.add("event5");
+        evt_names.add("event6");
+        evt_names.add("event7");
+
+        View rootView= inflater.inflate(R.layout.fragment_eventsfrag, container, false);
+        event_list=(RecyclerView)rootView.findViewById(R.id.rv_events);
+        eventadapter mAdapter=new eventadapter(evt_names);
+
+        RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(getContext());
+        event_list.setLayoutManager(layoutManager);
+
+        event_list.setAdapter(mAdapter);
+        mAdapter.notifyDataSetChanged();
+        event_list.setHasFixedSize(true);
+        return rootView;
     }
+
+
+    public class eventviewholder extends RecyclerView.ViewHolder
+    {
+
+        TextView tvName;
+
+        public eventviewholder(View itemView) {
+            super(itemView);
+            tvName= (TextView) itemView.findViewById(R.id.tvEventName);
+        }
+    }
+
+    public class eventadapter extends RecyclerView.Adapter<eventviewholder>
+    {
+
+        ArrayList<String>mlist;
+
+        public eventadapter(ArrayList<String> mlist) {
+            this.mlist = mlist;
+        }
+
+        @Override
+        public eventviewholder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater=LayoutInflater.from(parent.getContext());
+            View itemview;
+            eventviewholder viewHolder;
+            itemview=inflater.inflate(R.layout.eventcard,parent,false);
+            viewHolder=new eventviewholder(itemview);
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(eventviewholder holder, int position) {
+
+            holder.tvName.setText(mlist.get(position));
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mlist.size();
+        }
+    }
+
 
 }
