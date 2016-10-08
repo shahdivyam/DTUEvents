7495dcd9b3cc137a51ba2114c2a73c4c57edee4c
furyjack <lakshaytaneja26@gmail.com>
2016-10-08 23:54:31 +0530

    network listner added to event frag

--- app/src/main/java/com/shah/divyam/dtuevents/Fragments/eventsfrag.java
+++ app/src/main/java/com/shah/divyam/dtuevents/Fragments/eventsfrag.java
@@ -1,8 +1,13 @@
 package com.shah.divyam.dtuevents.Fragments;
 
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -34,6 +39,43 @@ public class eventsfrag extends Fragment {
     ArrayList<String>ev_url;
     RequestQueue queue;
 
+    private Boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager)getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
+    }
+
+    public Boolean isOnline() {
+        try {
+
+            final Process p1 = java.lang.Runtime.getRuntime().exec("ping -c 1 www.google.com");
+
+            int returnVal = p1.waitFor();
+            return (returnVal == 0);
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private void showAlert() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setMessage(R.string.dialog_message)
+                .setTitle(R.string.dialog_title);
+
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                getActivity().finish();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
 
     public eventsfrag() {
         // Required empty public constructor
@@ -46,70 +88,66 @@ public class eventsfrag extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        ev_list=new ArrayList<>();
-        ev_url=new ArrayList<>();
-        queue= Volley.newRequestQueue(getContext());
-       ev_url.add("https://api.myjson.com/bins/4zkr2");
-        ev_url.add("https://api.myjson.com/bins/4zkr2");
-        ev_url.add("https://api.myjson.com/bins/4zkr2");
-        ev_url.add("https://api.myjson.com/bins/4zkr2");
-        ev_url.add("https://api.myjson.com/bins/4zkr2");
-        ev_url.add("https://api.myjson.com/bins/4zkr2");
-        ev_url.add("https://api.myjson.com/bins/4zkr2");
-        queue.start();
-       final eventadapter mAdapter=new eventadapter(ev_list);
-
-        for(int i=0;i<ev_url.size();i++) {
-
-            String url = ev_url.get(i);
-            JsonObjectRequest req=new JsonObjectRequest(url, new Response.Listener<JSONObject>() {
-                @Override
-                public void onResponse(JSONObject response) {
-                    Event n=new Event();
-                    try {
-                        n.setEvname(response.getString("title"));
-                        n.setImgurl(response.getString("imageUrl"));
-                        n.setOrgan(response.getString("Organby"));
-                        n.setOn(response.getString("date"));
-                        ev_list.add(n);
-                        mAdapter.notifyDataSetChanged();
-                    }
-                    catch (JSONException e)
-                    {
-                        e.printStackTrace();
-                    }
-
-                }
-            },new Response.ErrorListener()
-            {
-
-                @Override
-                public void onErrorResponse(VolleyError error) {
-
-                }
-            });
-        queue.add(req);
-        }
-
-
-
-
         View rootView= inflater.inflate(R.layout.fragment_eventsfrag, container, false);
         event_list=(RecyclerView)rootView.findViewById(R.id.rv_events);
 
 
+        if(!isOnline() || !isNetworkAvailable())
+        {
+            showAlert();
+        }
+        else {
+
+            ev_list = new ArrayList<>();
+            ev_url = new ArrayList<>();
+            queue = Volley.newRequestQueue(getContext());
+            ev_url.add("https://api.myjson.com/bins/4zkr2");
+            ev_url.add("https://api.myjson.com/bins/4zkr2");
+            ev_url.add("https://api.myjson.com/bins/4zkr2");
+            ev_url.add("https://api.myjson.com/bins/4zkr2");
+            ev_url.add("https://api.myjson.com/bins/4zkr2");
+            ev_url.add("https://api.myjson.com/bins/4zkr2");
+            ev_url.add("https://api.myjson.com/bins/4zkr2");
+            queue.start();
+            final eventadapter mAdapter = new eventadapter(ev_list);
+
+            for (int i = 0; i < ev_url.size(); i++) {
+
+                String url = ev_url.get(i);
+                JsonObjectRequest req = new JsonObjectRequest(url, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Event n = new Event();
+                        try {
+                            n.setEvname(response.getString("title"));
+                            n.setImgurl(response.getString("imageUrl"));
+                            n.setOrgan(response.getString("Organby"));
+                            n.setOn(response.getString("date"));
+                            ev_list.add(n);
+                            mAdapter.notifyDataSetChanged();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+                queue.add(req);
+            }
 
 
+            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+            event_list.setLayoutManager(layoutManager);
 
-
-
-
-        RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(getContext());
-        event_list.setLayoutManager(layoutManager);
-
-        event_list.setAdapter(mAdapter);
-        mAdapter.notifyDataSetChanged();
-        event_list.setHasFixedSize(true);
+            event_list.setAdapter(mAdapter);
+            mAdapter.notifyDataSetChanged();
+            event_list.setHasFixedSize(true);
+        }
         return rootView;
     }
 
